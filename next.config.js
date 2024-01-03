b12e63503a90068fae27ec0033988c284917ae57
Ryan Tan <ryantan@rtyt16.local>
2024-01-03 16:09:30 +0800

    updated next.config.js to for deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: "export",
+};
 const withImages = require("next-images");
 
 module.exports = withImages();
