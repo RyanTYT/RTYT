00db80451270177177e991660f0e9fbcf6c5d4c1
Ryan Tan <ryantan@rtyt16.local>
2024-01-10 13:10:23 +0800

    fixed for github pages

--- next.config.js
+++ next.config.js
@@ -2,11 +2,12 @@
 
 const env = process.env.NODE_ENV;
 var repo;
-if (env === "development") {
-	repo = "";
-} else {
-	repo = "RTYT";
-}
+// if (env === "development") {
+// 	repo = "";
+// } else {
+// 	repo = "RTYT";
+// }
+repo = "RTYT";
 const assetPrefix = `/${repo}/`;
 const basePath = `/${repo}`;
 
