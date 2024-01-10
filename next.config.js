81373f0c5acfcd9c8d0b7c5b1e22240459745640
Ryan Tan <ryantan@rtyt16.local>
2024-01-10 13:13:14 +0800

    actually fixed for github pages

--- next.config.js
+++ next.config.js
@@ -2,18 +2,17 @@
 
 const env = process.env.NODE_ENV;
 var repo;
-// if (env === "development") {
-// 	repo = "";
-// } else {
-// 	repo = "RTYT";
-// }
-repo = "RTYT";
+if (env === "development") {
+	repo = "";
+} else {
+	repo = "RTYT";
+}
 const assetPrefix = `/${repo}/`;
 const basePath = `/${repo}`;
 
 const nextConfig = {
-	// basePath: basePath,
-	// assetPrefix: assetPrefix,
+	basePath: basePath,
+	assetPrefix: assetPrefix,
 	output: "export",
 	images: {
 		unoptimized: true,
