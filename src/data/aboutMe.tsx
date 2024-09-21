c61eab74f6de9235604f8cde23cba4359c18cfcf
Ryan Tan <ryantan@rtyt-m2-mac.local>
2024-09-21 21:49:44 +0800

    Updaed styling and added name

--- src/data/aboutMe.tsx
+++ src/data/aboutMe.tsx
@@ -14,26 +14,19 @@ export const aboutMeStr = `
                 background: linear-gradient(to right, var(--important-translucent), var(--important) 50%, transparent 50%); /* Gradient from yellow to transparent */
                 background-size: 200% 50%; /* Double the width of the background for animation */
                 background-position: 100% 50%; /* Start at the right */
-                animation: highlight 2s ease-in-out forwards; /* Infinite animation loop */
-                color: var(--flip-font-color);
                 padding: 0rem 0.3rem;
                 border-radius: 0.2rem;
+
+                color: transparent;
+                background-clip: text;
+                font-size: 1.5rem;
+                font-weight: 700;
+
+                animation: highlight 2s forwards; /* Infinite animation loop */
             }
 
-            <style>
-    @keyframes highlight {
-        0% { 
-            background-position: 100% 0; /* Start at the right */
-        }
-        100% { 
-            background-position: 0 0; /* End at the left */
-        }
-    }
-
-</style>
-
         </style>
-				Hi! I'm currently a <span class="impt highlighted">Year 3 undergraduate</span> studying a Double Degree Programme 
+				Hi! I'm <span class="impt highlighted">RTYT (Ryan Tan Yan Tong)</span> currently a <span class="impt highlighted">Year 3 undergraduate</span> studying a Double Degree Programme 
                 in Business Analytics (BZA) and Business Administration (BBA) in NUS
 				(National University of Singapore). For clarity, I'm under the School of 
                 Computing as well as the School of Business. After exploring this space, I 
@@ -41,5 +34,5 @@ export const aboutMeStr = `
                 (which also plays into why I decided to TA CS2040 - Data Structures and 
                 Algorithms). As such, certain applications of Machine Learning and AI, which
                 seem like amazing answers to probability questions, also greatly interest me.
-				Feel free to reach out to me via any of the links at the bottom!"
+				Feel free to reach out to me via any of the links at the bottom!
 `;
